602b4e046e54cf39047f4f6cba6a101ae5afc8d7
jianwei <1807176900@qq.com>
2022-10-10 20:37:41 +0800

    测试feat

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
-
+// test
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
